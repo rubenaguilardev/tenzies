73f64e6f04591aaec8b7bd7ef207ff4d3c9c7baf
rubenaguilardev <ragui56@wgu.edu>
2025-05-13 22:53:45 -0700

    add die component and generate random die

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,17 +1,26 @@
+import Die from './Die'
+import { useState } from 'react'
+
 export default function App() {
+
+    const [die, setDie] = useState(generateAllNewDice())
+
+    function generateAllNewDice() {
+        
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+    
+    const newDie = die.map(eachDie => <Die value={eachDie}/>)
+
+
     return (
         <main>
             <h1>Tenzies</h1>
             <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <section>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
+                {newDie}
             </section>
             <button className='rollBtn'>Roll</button>
 
